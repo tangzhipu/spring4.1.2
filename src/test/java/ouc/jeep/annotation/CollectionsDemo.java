28535a6cdcfc12191a8239b5a322088270497200
tangzhipu <327021108@qq.com>
2014-11-29 11:18:07 +0800

    20141129 annotation test

---
+++ src/test/java/ouc/jeep/annotation/CollectionsDemo.java
@@ -0,0 +1,19 @@
+package ouc.jeep.annotation;
+
+import java.util.*;
+public class CollectionsDemo {
+   public static void main(String args[]) {
+      // create array list object       
+      List arrlist = new ArrayList();
+      
+      // populate the list
+      arrlist.add("A");
+      arrlist.add("B");
+      arrlist.add("C");  
+      
+      System.out.println("Initial collection value: "+arrlist);
+      // add values to this collection
+      boolean b = Collections.addAll(arrlist, "1","2","3");
+      System.out.println("Final collection value: "+arrlist);
+   }    
+}
--- src/test/java/ouc/jeep/annotation/Test.java
+++
@@ -1,5 +0,0 @@
-package ouc.jeep.annotation;
-
-public class Test {
-
-}
